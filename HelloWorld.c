325d6cd367b027297fe7f002742ff7fb88682acb
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-14 00:25:32 +0300

    Learned comments and sequences

--- HelloWorld.c
+++ HelloWorld.c
@@ -3,7 +3,24 @@
 int main()
 {
 
+    // This a comment block.
+
+    /* This is a
+    multiline
+    comment
+    block.*/
+
+    /* escape sequence = character combination consisting of a backslash \
+                        followed by a letter or combination of digits.
+                        They specify actions within a line or string of text.
+                        \n = newline
+                        \t = tab
+    */
+
     printf("I like pizza!\n");
     printf("It's really good!\n");
+
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+    printf("\"I like Pizza\" - Mutai Kelvin");
     return 0;
 }
