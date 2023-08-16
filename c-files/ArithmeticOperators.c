237d51dc0ba5a3d881623a0d7d9ba658a66179a6
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-16 22:17:01 +0300

    Arithmetic opertors in C programming

---
+++ c-files/ArithmeticOperators.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // arithmetic operators
+
+    //  + (addition)
+    //  - (subtraction)
+    //  * (multiplication)
+    //  / (division)
+    //  % (modulus) Reminder of any division
+    //  ++ increment
+    //  -- decrement
+
+    //  int z = x + y;
+    //  int z = x - y;
+    //  int z = x * y;
+    //  int z = x / (float) y;
+
+    //  x++;
+    //  y--;
+
+    // Take note integer division
+
+    int x = 5;
+    int y = 2;
+
+    float z = x / (float)2;
+
+    printf("%f", z);
+
+    return 0;
+}
