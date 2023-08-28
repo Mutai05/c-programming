d434cea539b7defdbd6de3949633afda5b1199d9
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-28 15:37:30 +0300

    More on If else statements

--- c-files/intro-to-c/Ifelse.c
+++ c-files/intro-to-c/Ifelse.c
@@ -22,13 +22,90 @@ int testVariables(void)
 
 	if (x > y)
 	{
-		printf("X is greater than y\n");
+		printf("x is greater than y\n");
 	}
+}
+
+/*
+ * Use the else statement to specify a block of code
+ * to be executed if the condition is false.
+ */
+
+int greetingTimes(void)
+{
+	int time = 20;
+
+	if (time < 18)
+	{
+		printf("Good day\n");
+	}
+	else
+	{
+		printf("Good evening\n");
+	}
+}
+
+/*
+ * Use the else if statement to specify
+ * a new condition if the first condition is false.
+ *
+ */
+
+int printGreetings(void)
+{
+
+	int time = 15;
+
+	if (time < 11)
+	{
+		printf("Good morning!\n");
+	}
+	else if (time < 18)
+	{
+		printf("Good afternoon!\n");
+	}
+	else
+	{
+		printf("Good evening!\n");
+	}
+}
+
+int checkNumber(void)
+{
+
+	int myNum = 30;
+
+	if (myNum > 0)
+	{
+		printf("This is a positive number.\n\n");
+	}
+	else if (myNum < 0)
+	{
+		printf("This is a negative number.\n\n");
+	}
+	else
+	{
+		printf("The value is zero\n\n.");
+	}
+}
+
+/*
+ * Short Hand If...Else (Ternary Operator)
+ *
+ */
+int ShortHandIf(void)
+{
+	int time = 20;
+	(time < 18) ? printf("Good day.\n") : printf("Good evening.\n");
 }
 
 int main(void)
 {
 	ifElse();
 	testVariables();
+	greetingTimes();
+	printGreetings();
+	checkNumber();
+	ShortHandIf();
 	return (0);
 }
