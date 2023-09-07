1c72bbb599aeafe4d3d8850bb5626e6a17a27dab
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-09-07 23:59:11 +0300

    Arrays in C In C, you can declare an array as follows:

---
+++ c-files/intro-to-c/Arrays.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // Declare an array of integers with 5 elements
+    int myArray[5];
+    
+    // Initialize the elements of the array
+    myArray[0] = 10;
+    myArray[1] = 20;
+    myArray[2] = 30;
+    myArray[3] = 40;
+    myArray[4] = 50;
+
+    // Access and print values from the array
+    printf("Element 0: %d\n", myArray[0]);
+    printf("Element 1: %d\n", myArray[1]);
+    printf("Element 2: %d\n", myArray[2]);
+    printf("Element 3: %d\n", myArray[3]);
+    printf("Element 4: %d\n", myArray[4]);
+
+    return 0;
+}
