116421b388dda1e96b2547db71a9ec61d87c24b0
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-09-15 22:29:50 +0300

    Arrays in C

--- c-files/intro-to-c/Arrays.c
+++ c-files/intro-to-c/Arrays.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 
-int main() {
+int arraySize()
+{
+    int myNumbers[] = {25, 50, 75, 100};
+
+    printf("%d\n", myNumbers[0]);
+}
+
+int changeArray()
+{
+    int myNumber[] = {25, 50, 75, 100};
+    myNumber[0] = 33;
+
+    printf("%d\n\n", myNumber[0]);
+}
+
+int loopArrays()
+{
+    int myNumbers[] = {25, 50, 75, 100};
+    int i;
+
+    for (i = 0; i < 4; i++)
+    {
+        printf("%d\n\n", myNumbers[i]);
+    }
+}
+
+int arraySizes()
+{
     // Declare an array of integers with 5 elements
     int myArray[5];
-    
+
     // Initialize the elements of the array
     myArray[0] = 10;
     myArray[1] = 20;
@@ -17,6 +44,14 @@ int main() {
     printf("Element 2: %d\n", myArray[2]);
     printf("Element 3: %d\n", myArray[3]);
     printf("Element 4: %d\n", myArray[4]);
+}
 
-    return 0;
+int main()
+{
+    arraySize();
+    changeArray();
+    loopArrays();
+    arraySizes();
+
+    return (0);
 }
