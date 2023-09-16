7ba379de34459829e5ec174fcd4e1d163cb710e3
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-09-16 15:00:40 +0300

    Modify Strings Loop Through a String

--- c-files/intro-to-c/Strings.c
+++ c-files/intro-to-c/Strings.c
@@ -7,14 +7,58 @@
 
 int cStrings()
 {
-    char greetings[] = "Hello World!\n";
+    char greetings[] = "Hello World!";
 
-    printf("%s", greetings);
+    printf("%s\n", greetings);
+}
+
+/*
+ * Since strings are actually arrays in C, you can access a string by referring to its
+ * index number inside square brackets [].
+ * This example prints the first character (0) in greetings:
+ */
+
+int accessStrings()
+{
+    char greetings[] = "Hello World";
+
+    printf("%c\n", greetings[0]);
+}
+
+/*
+ * To change the value of a specific character in a string,
+ * refer to the index number, and use single quotes:
+ */
+
+int modifyStrings()
+{
+    char greetings[] = "Hello World!";
+    greetings[0] = 'J';
+
+    printf("%s\n", greetings);
+}
+
+/*
+ * You can also loop through the characters of a string, using a for loop:
+ */
+
+int loopString()
+{
+    char carName[] = "Volvo";
+    int i;
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("%c\n", carName[i]);
+    }
 }
 
 int main()
 {
     cStrings();
+    accessStrings();
+    modifyStrings();
+    loopString();
 
     return (0);
 }
