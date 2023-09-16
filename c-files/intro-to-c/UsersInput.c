0ba10bf623f328aa9956f37df0283f231144a595
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-09-16 19:18:51 +0300

    User Imputs

--- c-files/intro-to-c/UsersInput.c
+++ c-files/intro-to-c/UsersInput.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
+int userInput()
+{
+    int myNum;
+
+    printf("Type a number and press enter: \n");
+    scanf("%d", &myNum);
+
+    printf("Your number is:%d\n", myNum);
+}
+
+int multipleInputs()
+{
+    int myNumber;
+    char myChar;
+
+    printf("Type a number and a charcter and press enter: \n");
+    scanf("%d %c", &myNumber, &myChar);
+
+    printf("Your number is: %d\n", myNumber);
+    printf("Your char is: %c\n", myChar);
+}
+
 int main()
 {
+    userInput();
+    multipleInputs();
 
-    char name[25];
-    int age;
-
-    printf("\nWhat's your name?");
-    // scanf("%s", &name);
-    fgets(name, 25, stdin);
-    name[strlen(name) - 1] = '\0';
-
-    printf("\nHow old are you?");
-    scanf("%d", &age);
-
-    printf("\nHello %s, how are you?", name);
-    printf("\nYou are %d years old\n", age);
-
-    return 0;
+    return (0);
 }
