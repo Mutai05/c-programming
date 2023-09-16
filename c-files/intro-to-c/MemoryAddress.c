3e1cc5aebbf195a1ea28790163199fed65173cbe
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-09-16 20:18:07 +0300

    C Memory Address

---
+++ c-files/intro-to-c/MemoryAddress.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/*
+ * When a variable is created in C, a memory address is assigned to the variable.
+ *
+ * The memory address is the location of where the variable is stored on the computer.
+ *
+ * When we assign a value to the variable, it is stored in this memory address.
+ *
+ * To access it, use the reference operator (&), and the result represents where
+ *
+ * the variable is stored:
+ *
+ */
+
+int memoryAddress()
+{
+    int myAge = 27;
+
+    printf("%p\n", &myAge);
+
+    return (0);
+}
+
+int main()
+{
+    memoryAddress();
+
+    return (0);
+}
