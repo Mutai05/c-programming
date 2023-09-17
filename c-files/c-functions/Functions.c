c6376ca63d120fb38fdc641008c56f93f7dee7ce
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-09-17 16:25:09 +0300

    Functions in C Create a Function Call a Function

---
+++ c-files/c-functions/Functions.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+ * A function is a block of code which only runs when it is called.
+ *
+ * Create a Function
+ *
+ * Syntax
+ *
+ * Call a Function
+ *
+ */
+
+void myFunction()
+{
+    printf("I just got Excecuted!\n");
+}
+
+int main()
+{
+    myFunction();
+
+    return (0);
+}
