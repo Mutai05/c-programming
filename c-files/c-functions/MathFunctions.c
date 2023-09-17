cd27e592306ae2b24aa8a4971b981a020ee20bc4
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-09-17 21:53:33 +0300

    Math Functions

---
+++ c-files/c-functions/MathFunctions.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+/* There is also a list of math functions available,
+ * that allows you to perform mathematical tasks on numbers.
+ *
+ * To use them, you must include the math.h header file in your program:
+ */
+
+int main()
+{
+    printf("%f\n", sqrt(16));
+    return 0;
+}
