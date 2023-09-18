d454920d362002aba3660b00a6cadf4fdbca0b27
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-09-18 14:59:34 +0300

    Multiple Structures in C

--- c-files/c-structures/Structures.c
+++ c-files/c-structures/Structures.c
@@ -13,14 +13,21 @@ int main()
     /* Create a structure variable of myStructure called s1 */
 
     struct myStructure s1;
+    struct myStructure s2;
 
     /* Assign values to members of s1 */
     s1.myNum = 15;
     s1.myLetter = 'M';
 
+    s2.myNum = 25;
+    s2.myLetter = 'Q';
+
     /* Print values */
-    printf("My number is: %d\n", s1.myNum);
-    printf("My letter is: %c\n", s1.myLetter);
+    printf("S1 My number is: %d\n", s1.myNum);
+    printf("S1 My letter is: %c\n\n", s1.myLetter);
+
+    printf("S2 My number is: %d\n", s2.myNum);
+    printf("S2 My letter is: %c\n", s2.myLetter);
 
     return (0);
 }
