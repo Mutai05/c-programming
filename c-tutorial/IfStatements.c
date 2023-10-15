6305fbfa390ca53c17e909123d4a0a3e234d220b
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-10-16 00:28:18 +0300

    More If else statements

--- c-tutorial/IfStatements.c
+++ c-tutorial/IfStatements.c
@@ -15,9 +15,29 @@ int max(int num1, int num2)
     return result;
 }
 
+int nums(int nums1, int nums2, int nums3)
+{
+    int result;
+    if (nums1 >= nums2 && nums1 >= nums3)
+    {
+        result = nums1;
+    }
+    else if (nums2 >= nums1 && nums2 >= nums3)
+    {
+        result = nums2;
+    }
+    else
+    {
+        result = nums3;
+    }
+
+    return result;
+}
+
 int main()
 {
     printf("%d\n", max(40, 10));
+    printf("%d\n", nums(5, 10, 15));
 
     return 0;
 }
