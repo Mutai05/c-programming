baa47d24e85ae37044d1e05e6bd9706bf2feac53
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-10-15 15:23:35 +0300

    C tutorial Created main.c file Working on variiables

---
+++ c-tutorial/variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("There was once a man named John\n");
+    printf("He was 35 years old\n");
+    printf("He really liked the name John\n");
+    printf("But did not like being 35.\n");
+    return (0);
+}
