c2d67f6da72a21df7221c04ecd5d5945d0c930d5
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-10-15 23:36:38 +0300

    More Functions

--- c-tutorial/Functions.c
+++ c-tutorial/Functions.c
@@ -6,11 +6,19 @@ void sayHi()
     printf("Hello User\n");
 }
 
+void hello(char name[], int age)
+{
+    printf("Hello %s, you are %d\n", name, age);
+}
+
 int main()
 {
     printf("Top\n");
     sayHi();
     printf("Bottom\n");
+    hello("Kelv", 40);
+    hello("Ess", 36);
+    hello("Jacq", 32);
 
     return (0);
 }
