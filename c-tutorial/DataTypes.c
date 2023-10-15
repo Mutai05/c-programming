b2264b1882e05ba199a2a142f11667798a07a5f6
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-10-15 15:54:23 +0300

    Data types in C

---
+++ c-tutorial/DataTypes.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int age = 40;
+    double gpa = 35.0;
+    char grade = 'A';
+    char phrase[] = "Hello World!";
+
+    printf("Print Age: %d\n", age);
+    printf("Print GPA: %lf\n", gpa);
+    printf("Print Grade: %c\n", grade);
+    printf("Print Phrase: %s\n", phrase);
+
+    return (0);
+}
