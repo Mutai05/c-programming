7fc1224e866b38aecd121d6e3f23d2383e7dce7a
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-10-15 15:38:41 +0300

    Updating Data Types

--- c-tutorial/variables.c
+++ c-tutorial/variables.c
@@ -9,6 +9,8 @@ int main()
 
     printf("There was once a man named %s\n", charactersName);
     printf("He was %d years old\n", charactersAge);
+
+    charactersAge = 30;
     printf("He really liked the name %s\n", charactersName);
     printf("But did not like being %d.\n", charactersAge);
     return (0);
