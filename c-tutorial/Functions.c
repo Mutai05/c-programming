a9fa966f0a9177b76b4b53af89d33eb590fd00fd
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-10-15 23:28:03 +0300

    Functions in C

---
+++ c-tutorial/Functions.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void sayHi()
+{
+    printf("Hello User\n");
+}
+
+int main()
+{
+    printf("Top\n");
+    sayHi();
+    printf("Bottom\n");
+
+    return (0);
+}
