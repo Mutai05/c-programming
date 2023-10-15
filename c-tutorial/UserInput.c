cb3a0c04f997a39fc3c4b2dd11a49e2d2436d64c
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-10-15 17:55:00 +0300

    More on User input

--- c-tutorial/UserInput.c
+++ c-tutorial/UserInput.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int userAge()
 {
     int age;
     printf("Enter your Age: \n");
     scanf("%d", &age);
 
     printf("Your are %d years old.\n", age);
+
+    return (0);
+}
+
+int userName()
+{
+    char name[20];
+    printf("Enter your Name: \n");
+    scanf("%s", name);
+
+    printf("Your name is %s.\n", name);
+
+    return (0);
+}
+
+int main()
+{
+    userAge();
+    userName();
 
     return (0);
 }
